b992aafd76c43328a91a33212477cb2b6070444a
ansteisija-kat <katty93cat@gmail.com>
2024-10-01 03:53:52 +0300

    create a new dict for books with pandas, it will be easier to map with users

--- src/hw_4_files/file.py
+++ src/hw_4_files/file.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+import pandas as pd
 
 CSV_FILE_PATH = Path("/Users/niacris/Downloads/books.csv")
 JSON_FILE_PATH = Path("/Users/niacris/Downloads/users.json")
@@ -7,14 +8,7 @@ JSON_FILE_PATH = Path("/Users/niacris/Downloads/users.json")
 #f = open("result.json", "a")
 
 with open(CSV_FILE_PATH, 'r') as b:
-    books = []
-    for line in b:
-            # books = b.readline()
-            # print('books: ')
-            # print(books)
-        line = line.split('\n')[0]
-        books.append(line)
-            #print(line)
+    books = pd.read_csv(b, header=0).to_dict('records') # !!!
     print('books: ')
     print(books)
 
@@ -25,8 +19,11 @@ with open(JSON_FILE_PATH, 'r') as u:
     print('users: ')
     print(users)
 
+
+# TODO осталось раскидать книги по юзерам, книги замаплены как надо
+
 my_json_string = json.dumps({'name': users[0]["name"], 'gender': users[0]["gender"], 'address': users[0]["address"], 'age': users[0]["age"],
-                             'books': books[1]})
+                             'books': {...})
 
 print('my_json_string: ')
 print(my_json_string)
