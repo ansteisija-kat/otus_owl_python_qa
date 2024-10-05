5f716f0b18bc9ab4e6fadcf1ca9e1c0dfa7c212a
ansteisija-kat <katty93cat@gmail.com>
2024-10-06 00:22:26 +0300

    add sorted books for every user & creating result.json & small format changes for read files

--- src/hw_4_files/file.py
+++ src/hw_4_files/file.py
@@ -1,31 +1,53 @@
+import os.path
 import json
 from pathlib import Path
 import pandas as pd
 
-CSV_FILE_PATH = Path("/Users/niacris/Downloads/books.csv")
-JSON_FILE_PATH = Path("/Users/niacris/Downloads/users.json")
 
-#f = open("result.json", "a")
+CSV_FILE_PATH = Path(".../Downloads/books.csv")
+JSON_FILE_PATH = Path(".../Downloads/users.json")
+
 
 with open(CSV_FILE_PATH, 'r') as b:
-    books = pd.read_csv(b, header=0).to_dict('records') # !!!
-    print('books: ')
-    print(books)
+    books = pd.read_csv(b, header=0).to_dict('records')
+    all_books = []
+    for book in books:
+        all_books.append(
+            {
+                "title": book["Title"],
+                "author": book["Author"],
+                "pages": book["Pages"],
+                "genre": book["Genre"]
+            }
+        )
 
 
 with open(JSON_FILE_PATH, 'r') as u:
     ur = u.read()
     users = json.loads(ur)
-    print('users: ')
-    print(users)
+    all_users = []
+    for user in users:
+        all_users.append(
+            {
+                "name": user["name"],
+                "gender": user["gender"],
+                "address": user["address"],
+                "age": user["age"],
+                "books": []
+            }
+        )
 
+users_i = 0
+for i in range(len(all_books)):
+    all_users[users_i]["books"].append(all_books[i])
+    users_i += 1
 
-# TODO осталось раскидать книги по юзерам, книги замаплены как надо
+    if users_i == len(all_users):
+        users_i = 0
+        all_users[users_i]["books"].append(all_books[i])
+        users_i += 1
 
-my_json_string = json.dumps({'name': users[0]["name"], 'gender': users[0]["gender"], 'address': users[0]["address"], 'age': users[0]["age"],
-                             'books': {...})
-
-print('my_json_string: ')
-print(my_json_string)
-
+with open('.../Downloads/result.json', 'w') as result:
+    json_file = json.dumps(all_users)
+    result.write(json_file)
 
